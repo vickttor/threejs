99a8953625cf777512586196aa6e90bba8b18ce9
Victor Silva <victor.silvapc@hotmail.com>
2024-01-21 04:52:28 -0300

    feat: rendering another 3D object instead of suzanne

--- introduction/shapes/3DLoader.js
+++ introduction/shapes/3DLoader.js
@@ -1,5 +1,4 @@
 import * as THREE from "three";
-
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 export function render3DLoader() {
@@ -7,27 +6,27 @@ export function render3DLoader() {
   const renderer = new THREE.WebGLRenderer();
   const scene = new THREE.Scene();
   const camera = new THREE.PerspectiveCamera(
-    75,
+    50,
     window.innerWidth / window.innerHeight,
     0.1,
     1000
   );
-  camera.position.z = 7;
+  camera.position.z = 0.5;
+  camera.position.y = 0.1;
 
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.body.appendChild(renderer.domElement);
 
-  // AMBIENTE LIGHT
+  //   AMBIENTE LIGHT
   const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
   scene.add(ambientLight);
 
   // DIRECTIONAL LIGHT
-
   const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
   directionalLight.position.set(1, 1, 1).normalize();
   scene.add(directionalLight);
 
-  loader.load("/suzanne.glb", (gltf) => {
+  loader.load("/forest_house.glb", (gltf) => {
     scene.add(gltf.scene);
   });
 
